62ece516e184295241cd9bab4596c2499b4d356e
Hēsperus <hesprs@outlook.com>
2025-10-26 11:16:00 +0800

    try fix rofi

--- NixOS Configuration/home/rofi/default.nix
+++ NixOS Configuration/home/rofi/default.nix
@@ -1,6 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  programs.rofi.enable = true;
-  home.file.".config/rofi/config.rasi".source = ./config.rasi;
+  programs.rofi = {
+    enable = true;
+    theme = builtins.readfile ./styles.rasi;
+    settings = {
+      # The 'modi' option is a list of strings in Nix
+      modi = [ "drun" "filebrowser" "window" "run" ];
+      font = "Fira Sans 11";
+      show-icons = true;
+
+      # Display names for the modes
+      display-drun = " ";
+      display-run = " ";
+      display-filebrowser = "";
+      display-window = "";
+
+      drun-display-format = "{name}";
+
+      # Booleans are Nix booleans
+      hover-select = false;
+
+      # Numbers are Nix integers
+      scroll-method = 1;
+
+      # The me-select-entry and me-accept-entry options
+      me-select-entry = "";
+      me-accept-entry = "MousePrimary";
+
+      window-format = "{w} · {c} · {t}";
+    };
+  };
 }
